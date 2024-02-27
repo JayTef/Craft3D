884bd14cda288970d06afb7612174dd83340b244
Injae Cho <jaytef@Injaes-MacBook-Air.local>
2024-02-27 12:39:53 +0900

    MODIFIED

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3DCraft/",
-  // build: {
-  //   outDir: 'dist',
-  // },
+  base: "/Craft3D/",
 })
